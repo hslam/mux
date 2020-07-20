b4fe8e3db615c01a71a394163aa5f1b43848f97a
hslam <791874158@qq.com>
2020-07-20 12:15:22 +0800

    update method

--- mux.go
+++ mux.go
@@ -1,56 +1,50 @@
 package mux
 
 import (
-	"net/http"
-	"sync"
-	"strings"
 	"fmt"
+	"net/http"
+	"strings"
+	"sync"
 )
+
 const (
-	GET         = iota
+	OPTIONS = 1 << iota
+	GET
+	HEAD
 	POST
 	PUT
 	DELETE
-	PATCH
-	HEAD
-	OPTIONS
 	TRACE
 	CONNECT
+	PATCH
 )
 
 type Mux struct {
-	mut    		sync.RWMutex
-	prefixes	map[string]*Prefix
+	mut         sync.RWMutex
+	prefixes    map[string]*Prefix
 	middlewares []http.HandlerFunc
-	notFound 	http.HandlerFunc
-	groups 		map[string]*Mux
-	group		string
+	notFound    http.HandlerFunc
+	groups      map[string]*Mux
+	group       string
 }
-type Prefix struct {
-	m 			map[string]*Entry
-	prefix 		string
-}
-type Entry struct {
-	handler		http.HandlerFunc
-	key 		string
-	match 		[]string
-	params		map[string]string
-	get 		http.HandlerFunc
-	post 		http.HandlerFunc
-	put 		http.HandlerFunc
-	delete 		http.HandlerFunc
-	patch 		http.HandlerFunc
-	head     	http.HandlerFunc
-	options	 	http.HandlerFunc
-	trace	 	http.HandlerFunc
-	connect	 	http.HandlerFunc
 
+type Prefix struct {
+	m      map[string]*Entry
+	prefix string
+}
+
+type Entry struct {
+	handler http.HandlerFunc
+	key     string
+	match   []string
+	params  map[string]string
+	method  int
 }
 
 func New() *Mux {
 	m := &Mux{
 		prefixes: make(map[string]*Prefix),
-		groups: make(map[string]*Mux),
+		groups:   make(map[string]*Mux),
 	}
 	return m
 }
@@ -58,77 +52,63 @@ func New() *Mux {
 func newGroup(group string) *Mux {
 	m := &Mux{
 		prefixes: make(map[string]*Prefix),
-		groups: make(map[string]*Mux),
-		group:group,
+		groups:   make(map[string]*Mux),
+		group:    group,
 	}
 	return m
 }
+
 func (m *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		if err := recover(); err != nil {
-			http.Error(w,fmt.Sprint(err),http.StatusBadRequest)
+			http.Error(w, fmt.Sprint(err), http.StatusBadRequest)
 		}
 	}()
 	m.mut.RLock()
 	defer m.mut.RUnlock()
-	if m.serve(w,r){
+	if m.serveHTTP(w, r) {
 		return
-	}else {
-		for _,groupMux:=range m.groups{
-			if groupMux.serve(w,r){
+	} else {
+		for _, groupMux := range m.groups {
+			if groupMux.serveHTTP(w, r) {
 				return
 			}
 		}
 	}
-	if m.notFound!=nil{
-		m.notFound(w,r)
+	if m.notFound != nil {
+		m.notFound(w, r)
 		return
 	}
 	http.Error(w, "404 Not Found : "+r.URL.String(), http.StatusNotFound)
 }
-func (m *Mux) serve(w http.ResponseWriter, r *http.Request)bool {
-	path:=m.replace(r.URL.Path)
-	if entry:=m.getHandlerFunc(path);entry!=nil{
-		if r.Method=="GET"&&entry.get!=nil{
-			m.serveEntry(entry.get,w,r)
-			return true
-		}else if r.Method=="POST"&&entry.post!=nil{
-			m.serveEntry(entry.post,w,r)
-			return true
-		}else if r.Method=="PUT"&&entry.put!=nil{
-			m.serveEntry(entry.put,w,r)
-			return true
-		}else if r.Method=="DELETE"&&entry.delete!=nil{
-			m.serveEntry(entry.delete,w,r)
-			return true
-		}else if r.Method=="PATCH"&&entry.patch!=nil{
-			m.serveEntry(entry.patch,w,r)
-			return true
-		}else if r.Method=="HEAD"&&entry.head!=nil{
-			m.serveEntry(entry.head,w,r)
-			return true
-		}else if r.Method=="OPTIONS"&&entry.options!=nil{
-			m.serveEntry(entry.options,w,r)
-			return true
-		}else if r.Method=="TRACE"&&entry.trace!=nil{
-			m.serveEntry(entry.trace,w,r)
-			return true
-		}else if r.Method=="CONNECT"&&entry.connect!=nil{
-			m.serveEntry(entry.connect,w,r)
-			return true
-		}else if entry.isEmpty()&&entry.handler!=nil{
-			m.serveEntry(entry.handler,w,r)
+
+func (m *Mux) serveHTTP(w http.ResponseWriter, r *http.Request) bool {
+	path := m.replace(r.URL.Path)
+	if entry := m.getHandlerFunc(path); entry != nil {
+		if entry.method == 0 ||
+			r.Method == "GET" && entry.method&GET > 0 ||
+			r.Method == "POST" && entry.method&POST > 0 ||
+			r.Method == "PUT" && entry.method&PUT > 0 ||
+			r.Method == "DELETE" && entry.method&DELETE > 0 ||
+			r.Method == "PATCH" && entry.method&PATCH > 0 ||
+			r.Method == "HEAD" && entry.method&HEAD > 0 ||
+			r.Method == "OPTIONS" && entry.method&OPTIONS > 0 ||
+			r.Method == "TRACE" && entry.method&TRACE > 0 ||
+			r.Method == "CONNECT" && entry.method&CONNECT > 0 {
+			m.serveEntry(entry.handler, w, r)
 			return true
 		}
 	}
 	return false
 }
-func (m *Mux) serveEntry(handler http.HandlerFunc,w http.ResponseWriter, r *http.Request) {
-	m.middleware(w,r)
-	handler(w,r)
+
+func (m *Mux) serveEntry(handler http.HandlerFunc, w http.ResponseWriter, r *http.Request) {
+	m.middleware(w, r)
+	handler(w, r)
 }
-func (m *Mux) getHandlerFunc(path string) *Entry{
-	if prefix,key,ok:=m.matchParams(path);ok {
+
+func (m *Mux) getHandlerFunc(path string) *Entry {
+	if prefix, key, ok := m.matchParams(path); ok {
 		if entry, ok := m.prefixes[prefix].m[key]; ok {
 			return entry
 		}
@@ -136,85 +116,83 @@ func (m *Mux) getHandlerFunc(path string) *Entry{
 	return nil
 }
 
-func (m *Mux) HandleFunc(pattern string, handler http.HandlerFunc) *Entry{
+func (m *Mux) HandleFunc(pattern string, handler http.HandlerFunc) *Entry {
 	m.mut.RLock()
 	defer m.mut.RUnlock()
-	pattern=m.replace(pattern)
-	prefix,key,match,params:=m.parseParams(m.group+pattern)
+	pattern = m.replace(pattern)
+	prefix, key, match, params := m.parseParams(m.group + pattern)
 	if v, ok := m.prefixes[prefix]; ok {
 		if entry, ok := v.m[key]; ok {
-			entry.handler=handler
-			entry.key=key
-			entry.match=match
-			entry.params=params
+			entry.handler = handler
+			entry.key = key
+			entry.match = match
+			entry.params = params
 			m.prefixes[prefix].m[key] = entry
 			return entry
-		}else {
-			entry:=&Entry{}
-			entry.handler=handler
-			entry.key=key
-			entry.match=match
-			entry.params=params
+		} else {
+			entry := &Entry{}
+			entry.handler = handler
+			entry.key = key
+			entry.match = match
+			entry.params = params
 			m.prefixes[prefix].m[key] = entry
 			return entry
 		}
-	}else {
-		m.prefixes[prefix]=&Prefix{m:make(map[string]*Entry),prefix:prefix}
-		entry:=&Entry{}
-		entry.handler=handler
-		entry.key=key
-		entry.match=match
-		entry.params=params
+	} else {
+		m.prefixes[prefix] = &Prefix{m: make(map[string]*Entry), prefix: prefix}
+		entry := &Entry{}
+		entry.handler = handler
+		entry.key = key
+		entry.match = match
+		entry.params = params
 		m.prefixes[prefix].m[key] = entry
 		return entry
 	}
 }
 
-func (m *Mux) Group(group string,f func(m *Mux)){
+func (m *Mux) Group(group string, f func(m *Mux)) {
 	m.mut.RLock()
 	defer m.mut.RUnlock()
-	group=m.replace(group)
-	groupMux:=newGroup(group)
+	group = m.replace(group)
+	groupMux := newGroup(group)
 	f(groupMux)
-	for _,p:=range groupMux.prefixes{
-		for _,v:=range p.m{
-			v.FIN()
-		}
-	}
-	if _,ok:=m.groups[group];ok{
+	if _, ok := m.groups[group]; ok {
 		panic("Group Existed")
 	}
-	groupMux.middlewares=m.middlewares
-	m.groups[group]=groupMux
+	groupMux.middlewares = m.middlewares
+	m.groups[group] = groupMux
 }
-func (m *Mux) NotFound(handler http.HandlerFunc){
+
+func (m *Mux) NotFound(handler http.HandlerFunc) {
 	m.mut.RLock()
 	defer m.mut.RUnlock()
-	m.notFound=handler
+	m.notFound = handler
 }
-func (m *Mux) Use(handler http.HandlerFunc){
+
+func (m *Mux) Use(handler http.HandlerFunc) {
 	m.mut.RLock()
 	defer m.mut.RUnlock()
-	m.middlewares=append(m.middlewares,handler)
+	m.middlewares = append(m.middlewares, handler)
 }
-func (m *Mux) middleware(w http.ResponseWriter, r *http.Request){
-	for _,handler:=range m.middlewares{
-		handler(w,r)
+
+func (m *Mux) middleware(w http.ResponseWriter, r *http.Request) {
+	for _, handler := range m.middlewares {
+		handler(w, r)
 	}
 }
 
-func (m *Mux) Params(r *http.Request)(map[string]string){
+func (m *Mux) Params(r *http.Request) map[string]string {
 	m.mut.RLock()
 	defer m.mut.RUnlock()
-	params:=make(map[string]string)
-	path:=m.replace(r.URL.Path)
-	if prefix,key,ok:=m.matchParams(path);ok{
-		if entry,ok:=m.prefixes[prefix].m[key];ok{
-			strs := strings.Split(strings.Trim(path,prefix), "/")
-			if len(strs)==len(entry.match){
-				for i:=0;i<len(strs);i++{
-					if entry.match[i]!=""{
-						params[entry.match[i]]=strs[i]
+	params := make(map[string]string)
+	path := m.replace(r.URL.Path)
+	if prefix, key, ok := m.matchParams(path); ok {
+		if entry, ok := m.prefixes[prefix].m[key]; ok {
+			strs := strings.Split(strings.Trim(path, prefix), "/")
+			if len(strs) == len(entry.match) {
+				for i := 0; i < len(strs); i++ {
+					if entry.match[i] != "" {
+						params[entry.match[i]] = strs[i]
 					}
 				}
 			}
@@ -222,126 +200,124 @@ func (m *Mux) Params(r *http.Request)(map[string]string){
 	}
 	return params
 }
-func (m *Mux) matchParams(path string)(string,string,bool){
-	for _,p:=range m.prefixes{
-		if strings.HasPrefix(path,p.prefix){
-			for _,v:=range p.m{
-				r:=strings.TrimLeft(path,p.prefix)
-				if r==""{
-					return p.prefix,v.key,true
+
+func (m *Mux) matchParams(path string) (string, string, bool) {
+	for _, p := range m.prefixes {
+		if strings.HasPrefix(path, p.prefix) {
+			for _, v := range p.m {
+				r := strings.TrimLeft(path, p.prefix)
+				if r == "" {
+					return p.prefix, v.key, true
 				}
-				form:=strings.Split(r,"/")
-				if len(form)==len(v.match){
-					key:=""
-					for i:=0;i<len(form);i++ {
-						if v.match[i]!=""{
-							if i>0{
-								key+="/"
+				form := strings.Split(r, "/")
+				if len(form) == len(v.match) {
+					key := ""
+					for i := 0; i < len(form); i++ {
+						if v.match[i] != "" {
+							if i > 0 {
+								key += "/"
 							}
-						}else {
-							if i>0{
-								key+="/"+form[i]
-							}else {
-								key+=form[i]
+						} else {
+							if i > 0 {
+								key += "/" + form[i]
+							} else {
+								key += form[i]
 							}
 						}
 					}
-					if key==v.key{
-						return p.prefix,v.key,true
+					if key == v.key {
+						return p.prefix, v.key, true
 					}
 				}
 			}
 		}
 	}
-	return "","",false
+	return "", "", false
 }
-func (m *Mux) parseParams(pattern string)(string,string,[]string,map[string]string){
-	prefix:=""
+
+func (m *Mux) parseParams(pattern string) (string, string, []string, map[string]string) {
+	prefix := ""
 	var match []string
-	key:=""
-	params:=make(map[string]string)
-	if strings.Contains(pattern,":"){
-		idx:=strings.Index(pattern,":")
-		prefix=pattern[:idx]
+	key := ""
+	params := make(map[string]string)
+	if strings.Contains(pattern, ":") {
+		idx := strings.Index(pattern, ":")
+		prefix = pattern[:idx]
 		match = strings.Split(pattern[idx:], "/")
-		for i:=0;i<len(match);i++{
-			if strings.Contains(match[i],":"){
-				match[i]=strings.Trim(match[i],":")
-				params[match[i]]=""
-				if i>0{
-					key+="/"
+		for i := 0; i < len(match); i++ {
+			if strings.Contains(match[i], ":") {
+				match[i] = strings.Trim(match[i], ":")
+				params[match[i]] = ""
+				if i > 0 {
+					key += "/"
 				}
-			}else {
-				if i>0{
-					key+="/"+match[i]
-				}else {
-					key+=match[i]
+			} else {
+				if i > 0 {
+					key += "/" + match[i]
+				} else {
+					key += match[i]
 				}
-				match[i]=""
+				match[i] = ""
 			}
 		}
-	}else{
-		prefix=pattern
+	} else {
+		prefix = pattern
 	}
-	return prefix,key,match,params
+	return prefix, key, match, params
 }
-func (m *Mux) Once(){
-	m.mut.RLock()
-	defer m.mut.RUnlock()
-	for _,p:=range m.prefixes{
-		for _,v:=range p.m{
-			v.FIN()
-		}
-	}
-	for _,groupMux:=range m.groups{
-		groupMux.Once()
-	}
-}
+
 func (m *Mux) replace(s string) string {
-	for strings.Contains(s,"//"){
-		s=strings.ReplaceAll(s,"//","/")
+	for strings.Contains(s, "//") {
+		s = strings.ReplaceAll(s, "//", "/")
 	}
 	return s
 }
-func (entry *Entry) GET() *Entry{
-	entry.get=entry.handler
+
+func (entry *Entry) GET() *Entry {
+	entry.method |= GET
 	return entry
 }
-func (entry *Entry) POST() *Entry{
-	entry.post=entry.handler
+
+func (entry *Entry) POST() *Entry {
+	entry.method |= POST
 	return entry
 }
-func (entry *Entry) PUT()*Entry {
-	entry.put=entry.handler
+
+func (entry *Entry) PUT() *Entry {
+	entry.method |= PUT
 	return entry
 }
-func (entry *Entry) DELETE() *Entry{
-	entry.delete=entry.handler
+
+func (entry *Entry) DELETE() *Entry {
+	entry.method |= DELETE
 	return entry
 }
-func (entry *Entry) PATCH()*Entry {
-	entry.patch=entry.handler
+
+func (entry *Entry) PATCH() *Entry {
+	entry.method |= PATCH
 	return entry
 }
-func (entry *Entry) HEAD() *Entry{
-	entry.head=entry.handler
+
+func (entry *Entry) HEAD() *Entry {
+	entry.method |= HEAD
 	return entry
 }
-func (entry *Entry) OPTIONS() *Entry{
-	entry.options=entry.handler
+
+func (entry *Entry) OPTIONS() *Entry {
+	entry.method |= OPTIONS
 	return entry
 }
-func (entry *Entry) TRACE() *Entry{
-	entry.trace=entry.handler
+
+func (entry *Entry) TRACE() *Entry {
+	entry.method |= TRACE
 	return entry
 }
-func (entry *Entry) CONNECT() *Entry{
-	entry.connect=entry.handler
+
+func (entry *Entry) CONNECT() *Entry {
+	entry.method |= CONNECT
 	return entry
 }
-func (entry *Entry) FIN(){
-	entry.handler=nil
-}
+
 func (entry *Entry) All() {
 	entry.GET()
 	entry.POST()
@@ -352,20 +328,4 @@ func (entry *Entry) All() {
 	entry.DELETE()
 	entry.TRACE()
 	entry.CONNECT()
-	entry.FIN()
 }
-func (entry *Entry) isEmpty()bool{
-	if entry.get==nil&&
-		entry.post==nil&&
-			entry.head==nil&&
-				entry.options==nil&&
-					entry.put==nil&&
-						entry.patch==nil&&
-							entry.delete==nil&&
-								entry.trace==nil&&
-									entry.connect==nil{
-		return true
-	}
-	return false
-}
-
